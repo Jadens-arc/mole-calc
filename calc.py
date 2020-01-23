634f9da8f4e8f1671d4a4e91c460ec0937e3fcb4
jaden arceneaux <arceneauxjaden@gmail.com>
2020-01-23 10:07:18 -0800

    working on grams to moles

--- calc.py
+++ calc.py
@@ -5,7 +5,7 @@ class ElementCalc:
         with open(path, 'r') as rawData:
             self.elements = json.loads(rawData.read())
 
-    def save(self)
+    def save(self):
         with open(self.filepath, 'w') as savedata:
             savedata.wright(json.dumps(self.elements))
 
@@ -16,20 +16,53 @@ class ElementCalc:
         self.save()
         # saves it to json file
 
+    def gramToMole(self, elements, amount):
+        atomicMass = 0
+        for element in elements:
+            if element not in self.elements:
+                print('element non in system')
+                return 0
 
-
+            atomicMass += self.elements[element]
+        return (amount / atomicMass, atomicMass)
 
 if __name__ == '__main__':
     myEle = ElementCalc('elementdata.json')
+    features = {
+        'help or ?': 'for information about what you can do',
+        'mtg': 'moles to grams',
+        'gtm': 'grams to moles',
+        'add element': 'to add an element and it\'s attributes to our data set'
+    }
     while True:
-        userIn = input()
-        if userIn == 'add element':
+        userIn = input().strip().lower()
+        if userIn == 'help' or userIn == '?':
+            for feature, definition in features.items():
+                print(feature)
+                print('-- ' + definition)
+
+        elif userIn == 'add element':
             newElement = {
                 input('What is the symbol for the element\n'): {
-                        'name': input('What is the full name of the element\n')
+                        'name': input('What is the full name of the element\n'),
                         'atomic-mass': input('What is the atomic mass of the element\n')
                     }
             }
             myEle.addElement(newElement)
+
+        elif userIn == 'gtm':
+            userElements = []
+            userElementIn = input('What elements are present(e.x. HHO = H2O)\nSay done when you are done\n')
+            userElements.append(userElementIn)
+
+            while userElementIn != 'done':
+                userElementIn = input('What elements are present(e.x. HHO = H2O)\nSay done when you are done\n')
+                userElements.append(userElementIn.upper())
+
+            userMass = input('What is the total mass in grams\n')
+
+            answer = myEle.gramToMole(userElements, userMass)
+            print(str(answer) + 'mols')
+
         else:
             print('not in system')
